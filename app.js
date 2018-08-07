227523f887da70add765e248f465b1a3af9cad3a
nizarrk <niezar.rk@gmail.com>
2018-08-07 14:32:39 +0700

    axios async await

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 // const todoController = require('./controllers/todoController');
 const user = require("./controllers/user");
 const azure = require("./controllers/azure");
+const axios = require("./controllers/axiosTest");
 const app = express();
 
 //set up template engine
@@ -10,7 +11,7 @@ const app = express();
 //static file
 // app.use(express.static("public"));
 app.use("/azure", azure);
-
+app.use("/axios", axios);
 //fire controllers
 // todoController(app);
 user(app);
--- controllers/azure.js
+++ controllers/azure.js
@@ -2,11 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 var bodyParser = require("body-parser");
-var kudu = require("kudu-api")({
-    website: "https://dummy-hash.scm.azurewebsites.net/",
-    username: "$robi027",
-    password: "mnbvcxz12327"
-});
 var urlencodedParser = bodyParser.urlencoded({ extended: false});
 
 // module.exports = function(app){
@@ -14,12 +9,5 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false});
 //         res.send("hello");
 //     });
 // }
-
-router.get("/", function(req, res, next){
-    kudu.scm.info(function (err, info){
-        if (err) throw err;
-        console.log(err);
-    });
-});
 
 module.exports = router;
