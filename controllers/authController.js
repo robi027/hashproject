85b5faff0beee61930d6b869e0479bcb4890979f
nizarrk <niezar.rk@gmail.com>
2018-08-15 16:43:42 +0700

    auth done bosque

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,33 +18,39 @@ var config = require('../config'); // get config file
 
 router.post('/login', async function(req, res) {
   try {
-    var id;
-    var user;
-    var pass;
-    var passwordIsValid;
-    var snapshot = await userCollection.where('email', '==' , req.body.email).where('password', '==' , req.body.password).get();
+    var id, user, pass, passwordIsValid;
+    var snapshot = await userCollection.where('email', '==' , req.body.email).get();
     snapshot.forEach(doc => {
       user = doc.data().email;
       pass = doc.data().password;
-      console.log(user);
-      console.log(pass);
+      console.log('email: ' + user);
+      console.log('hashedpassword: ' + pass);
       id = doc.id;
-      console.log(id);
+      console.log('id: ' + id);
       
       // check if the password is valid
       passwordIsValid = bcrypt.compareSync(req.body.password, doc.data().password);      
     });
-    if (user && pass) {
-      console.log(req.body.email + ' found.');
-      console.log(req.body.password + ' found.');
-      res.status(200).send(req.body.email + ' found.');
-      res.status(200).send(req.body.password + ' found.');
+    if (user && passwordIsValid) {
+      console.log(req.body.email + ' input found.');
+      console.log(req.body.password + ' input found.');
+      
+      // if user is found and password is valid
+    // create a token
+    var token = jwt.sign({ id: id }, config.secret, {
+      expiresIn: 86400 // expires in 24 hours
+    });
+
+    // return the information including token as JSON
+    return res.status(200).send({ auth: true, token: token });
+    //return res.status(200).send('name: ' + req.body.email + ' found.\n password: ' + req.body.password + ' found.');
     } else if (!user) {
       console.log('No user found.');
-      res.status(404).send('No user found.');
+      return res.status(404).send('No user found.');
     } else if (!passwordIsValid) { 
-      res.status(401).send({ auth: false, token: null });
+      return res.status(401).send({ auth: false, token: null });
     }
+
   
 
   } catch (error) {
@@ -81,14 +87,15 @@ router.post('/register', async function(req, res) {
       };
 });
 
-router.get('/me', verifyToken, function(req, res, next) {
-
-  User.findById(req.userId, { password: 0 }, function (err, user) {
-    if (err) return res.status(500).send("There was a problem finding the user.");
-    if (!user) return res.status(404).send("No user found.");
-    res.status(200).send(user);
-  });
-
+router.get('/me', verifyToken, async function(req, res, next) {
+  try {
+    const response = await userCollection.doc(req.userId).get();
+    res.send(response.data());
+    console.log(response.id);
+    console.log(response.data());
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 module.exports = router;
