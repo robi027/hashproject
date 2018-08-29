d956e3e9253c3b1db9bbcb35d30ef05ec919d116
nizarrk <niezar.rk@gmail.com>
2018-08-29 20:54:19 +0700

    azure progress

--- controllers/azure.js
+++ controllers/azure.js
@@ -1,17 +1,22 @@
 const express = require("express");
 var axios = require("axios");
 const router = express.Router();
+var bodyParser = require("body-parser")
+router.use(bodyParser.json());
 
 
 const deployments = "https://dummy-hash.scm.azurewebsites.net/api/deployments";
 const logstream = "https://dummy-hash.scm.azurewebsites.net/api/logstream";
-// const username = "$robi027";
-// const password = "mnbvcxz12327";
-var header = {
-  headers: {
-    "Authorization" : "Basic cm9iaTAyNzptbmJ2Y3h6MTIzMjc="
-  }
-}
+// var username = req.body.username;
+// var pass = req.body.password;
+// var encode = username + ":" + pass;
+// var buff = new Buffer(encode);  
+// var base64data = buff.toString('base64');
+// var header = {
+//   headers: {
+//     "Authorization" : "Basic "
+//   }
+// }
 
 router.get("/deployments/", async (req, res, next) => {
   try {
@@ -23,10 +28,37 @@ router.get("/deployments/", async (req, res, next) => {
   next();
 })
 
-router.get("/logstream/", async (req, res, next) =>{
+router.get("/logstream/", async (req, res, next) => {
   try {
     var response = await axios.get(logstream, header)
     res.send(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+})
+
+router.post("/encode", async (req, res, next) => {
+  try {
+    var username = req.body.username;
+    var pass = req.body.password;    
+    var encode = username + ":" + pass;
+    console.log(encode);
+    var buff = new Buffer(encode);  
+    var base64data = buff.toString('base64');
+    console.log(base64data);
+    res.send('"' + encode + '" converted to Base64 is "' + base64data + '"'); 
+  } catch (error) {
+    res.send(error);
+  }
+})
+
+router.get("/decode/:data", async (req, res, next) => {
+  try {
+    let data = req.params.data;  
+    let buff = new Buffer(data, 'base64');  
+    let text = buff.toString('ascii');
+
+    res.send('"' + data + '" converted from Base64 to ASCII is "' + text + '"');   
   } catch (error) {
     console.error(error);
   }
